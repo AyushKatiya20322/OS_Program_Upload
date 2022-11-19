6dd5837bcb9d7a351c4f701b2a1ac3ea1c7f7023
ayush katiya <112498869+AyushKatiya20322@users.noreply.github.com>
2022-11-19 11:00:47 +0530

    Update fcfs1.cpp

--- fcfs1.cpp
+++ fcfs1.cpp
@@ -1,70 +1,67 @@
-#include<iostream>
-using namespace std;
-class FCFS
-{
-    public:
-    
-    void findCompletionTime(int process[],int n,int burst_time[],int comp_time[])
-    {
-       
-        comp_time[0]=burst_time[0];
-        cout<<"Completion Time:"<<endl;
-        cout<<comp_time[0]<<endl;
-        for(int i=1;i<n;i++)
-        {
-            comp_time[i]=comp_time[i-1]+burst_time[i];
-            cout<<comp_time[i]<<endl;
-        }
-    }
-    void findTurnAroundTime(int process[],int n,int ct[])
-    {   
-        int tat[n],average;
-        cout<<"Turn around time:"<<endl;
-        for(int i=0;i<n;i++)
-        {
-            tat[i]=ct[i];
-            cout<<tat[i]<<endl;
-            average=average+tat[i];
-        }
-        cout<<"Average Turn around time:"<<average/n<<endl;
-    }
-    void findWaitingTime(int process[],int n,int ct[],int bt[],int wt[])
-    {   
-        int tat[n],average=0;
-        cout<<"Waiting Time:"<<endl;
-        for(int i=0;i<n;i++)
-        {
-             tat[i]=ct[i];
-            wt[i]=tat[i]-bt[i];
-            average=average+wt[i];
-            cout<<wt[i]<<endl;
-        }
-        average=average/n;
-        cout<<"Average waiting time:"<<average;
-    }
-};
-int main()
-{
-    FCFS f;
-    int n;
-    cout<<"Enter no of processes:";
-    cin>>n;
-    int process[n];
-    for(int i=1;i<=n;i++)
-    {
-        cin>>process[i];
-    } 
-    // cout<<"Process No.:"<<endl;
-    // for(int i=0;i<n;i++)
-    // {
-    //     cout<<process[i]<<endl;
-    // }
-    int burst_time[]={2,3,4,5};
-    int comp_time[n],waiting_time[n],tat[n];
-    f.findCompletionTime(process,n,burst_time,comp_time);
-    f.findTurnAroundTime(process,n,comp_time);
-    f.findWaitingTime(process,n,comp_time,burst_time,waiting_time);
-    
+#include <iostream>
+	using namespace std;
+	class FCFS
+	{
+	public:
+	void completion(int procesid[],int n,int bursttime[],int comp[])
+	{
+	    comp[0]=bursttime[0];
+	    for(int i=1;i<n;i++)
+	    {
+	        comp[i]=comp[i-1]+bursttime[i];
+	    }
+	}
+	
 
+	void turnaround_time(int procesid[],int n,int bursttime[],int tat[],int comp[])
+	{
+	   
+	    for(int i=0;i<n;i++)
+	    {
+	        tat[i]=comp[i];
+	    }
+	}
+	void waiting_time(int procesid[],int n,int bursttime[],int tat[],int wait[])
+	{
+	    for(int i=0;i<n;i++)
+	    {
+	        wait[i]=tat[i]-bursttime[i];
+	    }
+	}
+	void display(int procesid[],int comp[],int tat[],int wait[],int n)
+	{
+	    cout<<"p_ID completion TAT Waiting"<<endl;
+	    for(int i=0;i<n;i++)
+	    {
+	        cout<<procesid[i]<<"      " <<comp[i]<<"         "<<tat[i]<<"     "<<wait[i]<<endl;
+	    }
+	}
+	};
+	int main()
+	{
+	    FCFS f;
+	      int n;
+	      cout<<"Enter the number of process:";
+	      cin>>n;
+	      int procesid[n];
+	      int bursttime[n];
+	     // int arr[n]={0};
+	      for(int i=0;i<n;i++)
+	      {
+	          cout<<"Enter the process ID and burst time:";
+	          cin>>procesid[i]>>bursttime[i];
+	      }
+	      int comp[n];
+	      int tat[n];
+	      int wait[n];
+	     
+	     f. completion( procesid, n, bursttime,comp);
+	     
+	 
+	      f.turnaround_time( procesid, n, bursttime, tat,comp);
+	   f.waiting_time( procesid, n, bursttime, tat,wait);
+	  f.display( procesid,comp,tat, wait, n);
+	
 
-}
+	    return 0;
+	}
